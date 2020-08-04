1b229170b36e006d83c0a6062025ada394335b31
ltreven <ltreven@gmail.com>
2020-08-04 21:51:09 +0200

    persistiendo el estado en el localstorage

--- src/App.js
+++ src/App.js
@@ -11,57 +11,67 @@ import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
 import Profile from './components/Profile';
 import ProtectedRoute from './auth/protected-route';
+import Logout from './components/auth/Logout';
 
 class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { loggedInUser: null };
+    this.state = {
+      loggedInUser: JSON.parse(localStorage.getItem('loggedInUser')) || null
+    }
   }
 
   getTheUser = (userObj) => {
     this.setState({
       loggedInUser: userObj
     })
+    this.setState({
+      loggedInUser: userObj
+    }, () => {
+      localStorage.setItem('loggedInUser', JSON.stringify(this.state.loggedInUser))
+    })
   }
 
   render() {
     return (
       <div>
-          <Navbar user={this.state.loggedInUser} key={this.state.loggedInUser} />
-          <Switch>
-            <Route exact path="/">
-              <h1>Esta es la homepage</h1>
-            </Route>
-            <Route exact path="/projects/" render={(props) => <ProjectList {...props} 
-                                                            user={this.state.loggedInUser}
-                                                            key={this.state.loggedInUser} />
-                                                } />
-            <Route path="/projects/:id" render={(props) => <ProjectDetails {...props} 
-                                                            user={this.state.loggedInUser}/>
-                                                } />
-            <Route exact path="/map">
-              <Map />
-            </Route>
-            <Route exact path="/fileupload">
-              <FileUpload />
-            </Route>
-            <Route path="/signup" render={props => <Signup {...props} callback={this.getTheUser}  />} />
-            <Route path="/login" render={props => <Login {...props} callback={this.getTheUser}  />} />
+        <Navbar user={this.state.loggedInUser} key={this.state.loggedInUser} />
+        <Switch>
+          <Route exact path="/">
+            <h1>Esta es la homepage</h1>
+          </Route>
+          <Route exact path="/projects/" render={(props) => <ProjectList {...props}
+            user={this.state.loggedInUser}
+            key={this.state.loggedInUser} />
+          } />
+          <Route path="/projects/:id" render={(props) => <ProjectDetails {...props}
+            user={this.state.loggedInUser} />
+          } />
+          <Route exact path="/map">
+            <Map />
+          </Route>
+          <Route exact path="/fileupload">
+            <FileUpload />
+          </Route>
 
-            <ProtectedRoute 
-              path="/profile" 
-              user={this.state.loggedInUser}
-              component={Profile}
-              />
+          <Route path="/signup" render={props => <Signup {...props} callback={this.getTheUser} />} />
+          <Route path="/login" render={props => <Login {...props} callback={this.getTheUser} />} />
 
-            <ProtectedRoute 
-              path="/dashboard" 
-              user={this.state.loggedInUser}
-              component={Profile}
-              />
-              
-          </Switch>
+          <Route
+            exact
+            path="/logout"
+            render={(props) => <Logout {...props} callback={this.getTheUser} />}
+          />
+          <ProtectedRoute
+            path="/profile"
+            user={this.state.loggedInUser}
+            component={Profile}
+          />
+
+
+
+        </Switch>
       </div>
     );
 
